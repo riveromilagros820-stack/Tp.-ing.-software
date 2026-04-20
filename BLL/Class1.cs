a5f05ec804f00e0bce342ffc8658e45ab1eae06b
Milagros Rivero <riveromilagros820@gmail.com>
2026-04-20 11:07:58 -0300

    first commit

---
+++ BLL/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DAL;
+using MPP;
+
+
+namespace BLL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tp ing.software. Rivero - D'argenzio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BE;
+using DAL;
+using MPP;
+
+namespace Tp_ing.software.Rivero___D_argenzio
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
