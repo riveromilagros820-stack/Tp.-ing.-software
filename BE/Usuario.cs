7c662779a7140da85d030d12fc7ed1bc6bf09d63
maguid31 <maguidargen31@gmail.com>
2026-04-20 12:24:07 -0300

    Agregar clase Usuario y fmrLogin

---
+++ BE/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+    }
+}
