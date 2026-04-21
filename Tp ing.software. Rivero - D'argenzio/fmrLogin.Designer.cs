2f0796c992a5270ca8c773b74dc67cfb8f91ba6d
maguid31 <maguidargen31@gmail.com>
2026-04-21 18:58:13 -0300

    Formulario Login

--- Tp ing.software. Rivero - D'argenzio/fmrLogin.Designer.cs
+++ Tp ing.software. Rivero - D'argenzio/fmrLogin.Designer.cs
@@ -2,167 +2,170 @@
 {
     partial class fmrLogin
     {
-        /// <summary>
-        /// Variable del diseñador necesaria.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.CheckBox cbMostrarContraseña;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+          /// <summary>
+/// Variable del diseñador necesaria.
+/// </summary>
+private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpiar los recursos que se estén usando.
-        /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+/// <summary>
+/// Limpiar los recursos que se estén usando.
+/// </summary>
+/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+protected override void Dispose(bool disposing)
+{
+if (disposing && (components != null))
+{
+components.Dispose();
+}
+base.Dispose(disposing);
+}
 
-        #region Código generado por el Diseñador de Windows Forms
+#region Código generado por el Diseñador de Windows Forms
 
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
-        private void InitializeComponent()
-        {
+/// <summary>
+/// Método necesario para admitir el Diseñador. No se puede modificar
+/// el contenido de este método con el editor de código.
+/// </summary>
+private void InitializeComponent()
+{
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.cbMostrarContraseña = new System.Windows.Forms.CheckBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnIniciarSesion
             // 
-<<<<<<< HEAD
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.MediumSlateBlue;
-            this.label1.Location = new System.Drawing.Point(87, 82);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "INICIAR SESIÓN";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.btnIniciarSesion.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnIniciarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciarSesion.ForeColor = System.Drawing.Color.White;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(103, 374);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(158, 30);
+            this.btnIniciarSesion.TabIndex = 31;
+            this.btnIniciarSesion.Text = "CREAR CUENTA";
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
             // 
-            // label2
+            // label4
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 144);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 23);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Usuario";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(136, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "No tengo cuenta";
             // 
-            // txtUsuario
+            // btnRegistrar
             // 
-            this.txtUsuario.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtUsuario.Location = new System.Drawing.Point(28, 164);
-            this.txtUsuario.Multiline = true;
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(216, 28);
-            this.txtUsuario.TabIndex = 2;
-            // 
-            // txtContraseña
-            // 
-            this.txtContraseña.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.txtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtContraseña.Location = new System.Drawing.Point(28, 249);
-            this.txtContraseña.Multiline = true;
-            this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(216, 28);
-            this.txtContraseña.TabIndex = 4;
-            this.txtContraseña.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 229);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(99, 23);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Contraseña";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.btnRegistrar.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnRegistrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrar.ForeColor = System.Drawing.Color.White;
+            this.btnRegistrar.Location = new System.Drawing.Point(74, 254);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(216, 35);
+            this.btnRegistrar.TabIndex = 27;
+            this.btnRegistrar.Text = "INGRESAR";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
             // cbMostrarContraseña
             // 
             this.cbMostrarContraseña.AutoSize = true;
             this.cbMostrarContraseña.Cursor = System.Windows.Forms.Cursors.Hand;
             this.cbMostrarContraseña.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cbMostrarContraseña.Location = new System.Drawing.Point(94, 283);
+            this.cbMostrarContraseña.Location = new System.Drawing.Point(176, 184);
             this.cbMostrarContraseña.Name = "cbMostrarContraseña";
-            this.cbMostrarContraseña.Size = new System.Drawing.Size(182, 27);
-            this.cbMostrarContraseña.TabIndex = 5;
+            this.cbMostrarContraseña.Size = new System.Drawing.Size(114, 17);
+            this.cbMostrarContraseña.TabIndex = 26;
             this.cbMostrarContraseña.Text = "Mostrar contraseña";
             this.cbMostrarContraseña.UseVisualStyleBackColor = true;
             // 
-            // btnIngresar
+            // txtContraseña
             // 
-            this.btnIngresar.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIngresar.ForeColor = System.Drawing.Color.White;
-            this.btnIngresar.Location = new System.Drawing.Point(29, 342);
-            this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(216, 35);
-            this.btnIngresar.TabIndex = 6;
-            this.btnIngresar.Text = "INGRESAR";
-            this.btnIngresar.UseVisualStyleBackColor = false;
-            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            this.txtContraseña.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContraseña.Location = new System.Drawing.Point(74, 150);
+            this.txtContraseña.Multiline = true;
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(216, 28);
+            this.txtContraseña.TabIndex = 25;
             // 
-            // label4
+            // label3
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(80, 422);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(143, 23);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "No tengo cuenta";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Contraseña";
             // 
-            // label5
+            // txtUsuario
             // 
-            this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.Color.MediumSlateBlue;
-            this.label5.Location = new System.Drawing.Point(91, 449);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(111, 23);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Crear cuenta";
+            this.txtUsuario.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuario.Location = new System.Drawing.Point(74, 78);
+            this.txtUsuario.Multiline = true;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(216, 28);
+            this.txtUsuario.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label1.Location = new System.Drawing.Point(133, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "INICIAR SESIÓN";
             // 
             // fmrLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(289, 489);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(354, 434);
+            this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.cbMostrarContraseña);
             this.Controls.Add(this.txtContraseña);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "fmrLogin";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.fmrLogin_Load);
-=======
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
->>>>>>> ad4827051cbb22634af0befcd05a87be979e3a56
+            this.Load += new System.EventHandler(this.fmrLogin_Load_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
-        }
-
-        #endregion
-    }
 }
 
+#endregion
+}
+}
+
+    
--- Tp ing.software. Rivero - D'argenzio/fmrLogin.cs
+++ Tp ing.software. Rivero - D'argenzio/fmrLogin.cs
@@ -21,11 +21,6 @@ namespace Tp_ing.software.Rivero___D_argenzio
         }
 
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void label3_Click(object sender, EventArgs e)
         {
 
@@ -38,7 +33,7 @@ namespace Tp_ing.software.Rivero___D_argenzio
 
 
 
-        private void btnIngresar_Click(object sender, EventArgs e)
+       /* private void btnIngresar_Click(object sender, EventArgs e)
         {
             string apellido = txtUsuario.Text.Trim();
             string pass = txtContraseña.Text.Trim();
@@ -67,11 +62,207 @@ namespace Tp_ing.software.Rivero___D_argenzio
 
 
 
-        }
+        } */
 
         private void fmrLogin_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /*private void InitializeComponent()
+        {
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.txtConfirmContraseña = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.cbMostrarContraseña = new System.Windows.Forms.CheckBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnIniciarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciarSesion.ForeColor = System.Drawing.Color.White;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(96, 386);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(158, 30);
+            this.btnIniciarSesion.TabIndex = 31;
+            this.btnIniciarSesion.Text = "Iniciar Sesion";
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
+            // 
+            // txtConfirmContraseña
+            // 
+            this.txtConfirmContraseña.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtConfirmContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtConfirmContraseña.Location = new System.Drawing.Point(74, 224);
+            this.txtConfirmContraseña.Multiline = true;
+            this.txtConfirmContraseña.Name = "txtConfirmContraseña";
+            this.txtConfirmContraseña.Size = new System.Drawing.Size(216, 28);
+            this.txtConfirmContraseña.TabIndex = 30;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Confirmar contraseña";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 360);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Ya tengo cuenta";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnRegistrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrar.ForeColor = System.Drawing.Color.White;
+            this.btnRegistrar.Location = new System.Drawing.Point(64, 291);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(216, 35);
+            this.btnRegistrar.TabIndex = 27;
+            this.btnRegistrar.Text = "REGISTRARME";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            // 
+            // cbShowPassword
+            // 
+            this.cbMostrarContraseña.AutoSize = true;
+            this.cbMostrarContraseña.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbMostrarContraseña.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbMostrarContraseña.Location = new System.Drawing.Point(140, 258);
+            this.cbMostrarContraseña.Name = "cbShowPassword";
+            this.cbMostrarContraseña.Size = new System.Drawing.Size(114, 17);
+            this.cbMostrarContraseña.TabIndex = 26;
+            this.cbMostrarContraseña.Text = "Mostrar contraseña";
+            this.cbMostrarContraseña.UseVisualStyleBackColor = true;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContraseña.Location = new System.Drawing.Point(74, 150);
+            this.txtContraseña.Multiline = true;
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(216, 28);
+            this.txtContraseña.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuario.Location = new System.Drawing.Point(74, 78);
+            this.txtUsuario.Multiline = true;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(216, 28);
+            this.txtUsuario.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label1.Location = new System.Drawing.Point(133, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "INICIAR SESIÓN";
+            // 
+            // fmrLogin
+            // 
+            this.ClientSize = new System.Drawing.Size(354, 434);
+            this.Controls.Add(this.btnIniciarSesion);
+            this.Controls.Add(this.txtConfirmContraseña);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.cbMostrarContraseña);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "fmrLogin";
+            this.Load += new System.EventHandler(this.fmrLogin_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        } */
+
+        private void fmrLogin_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string apellido = txtUsuario.Text.Trim();
+            string pass = txtContraseña.Text.Trim();
+
+            int dni;
+            if (!int.TryParse(pass, out dni))
+            {
+                MessageBox.Show("La contraseña debe ser numérica (DNI).");
+                return;
+            }
+
+
+            Usuario u = BLLLogin.Instancia.ValidarLogin(apellido, dni, pass);
+
+            if (u != null)
+            {
+                MessageBox.Show("Login correcto. Debe cambiar su contraseña.");
+                frmCambioContraseña cambio = new frmCambioContraseña(u);
+                cambio.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.");
+            }
+
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            // Instanciamos el formulario de registro
+            fmrRegistro formularioRegistro = new fmrRegistro();
+
+            // Mostramos el formulario de registro
+            formularioRegistro.Show();
+
+            // Opcional: Ocultamos el formulario de login actual
+            this.Hide();
         }
     }
 
